ab8665fd8ebca9044493c398032e41a4eb0d0536
princesinff <166802071+princesinff@users.noreply.github.com>
2025-02-06 15:41:37 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "21722474"
+# -------------------------------------------------------------
+API_HASH = "ff2286ddf5b371eb23c571e7ff0c8b8d"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "7096860602"))
+SUPPORT_GRP = "@APNA_CLUB_09"
+UPDATE_CHNL = "SWEETY_BOT_UPDATE"
+OWNER_USERNAME = "PRINCE_WEBZ"
+    
